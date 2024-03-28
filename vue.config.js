900f619e5c372e227a3e32122fe54673f10f6ae4
Jaydan Huang <Jaydan@fakemail.com>
2024-03-28 23:28:31 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: "/wasa-jaydan/dist/",
+  publicPath: "/wasa.github.io/dist/",
 });
